ff2f912835d23b4802e45c9a4ecaefaa38ec6d87
Veron3033 <veronika.titova@edu.rtu.lv>
2026-03-04 20:51:16 +0000

    Implement holiday bonus calculation based on years worked

---  Flow Control/HolidayBonus.py
+++  Flow Control/HolidayBonus.py
@@ -2,4 +2,7 @@ salary = float(input("Your monthly salary: "))
 years_worked = int(input("Number of years worked: "))
 
 if years_worked > 2:
-    print("Your holiday bonus is: 15%")
+    bonus_years = years_worked - 2
+    bonus = salary * 0.15 * bonus_years
+    print(f"Your holiday bonus is: {bonus:.2f}")
+else:   print("You are not eligible for a holiday bonus.")
